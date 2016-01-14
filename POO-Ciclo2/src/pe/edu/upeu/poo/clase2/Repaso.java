15f666db344b03192f85b849f5f75f8b6a1f467c
javatarupeu <javatar.sempai@gmail.com>
2016-01-14 11:38:20 -0500

    Agregando metodos para calcular el IGV

--- POO-Ciclo2/src/pe/edu/upeu/poo/clase2/Repaso.java
+++ POO-Ciclo2/src/pe/edu/upeu/poo/clase2/Repaso.java
@@ -3,8 +3,47 @@ package pe.edu.upeu.poo.clase2;
 public class Repaso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Repaso r = new Repaso();
+
+		int a = 25;
+		int b = 144;
+
+		/**
+		 * Suma + Resta - Multiplicacion * Division / Modulo % Clase para ayuda
+		 * Matematica "Math"
+		 */
+
+		// Esta instruccion me sirve para imprimir en la consola
+		System.out.println("La suma es: " + r.sumar(a, b));
+
+		System.out.println("Operaciones Logicas");
+
+		/**
+		 * And && Or || Alt + 124 Negacion !
+		 */
+
+		System.out.println(!true);
+		
+		
+		System.out.println("Calculando el IGV");
+		System.out.println("IGV: "+r.calcIGV(100));
 
 	}
+
+	public int sumar(int sum1, int sum2) {
+
+		return sum1 + sum2;
+	}
+
+	public double calcIGV(double monto) {
+		double igv = 18;
+
+		return monto * igv / 100;
+	}
+	
+//	public double calcIMC(){
+//		
+//	}
 
 }
