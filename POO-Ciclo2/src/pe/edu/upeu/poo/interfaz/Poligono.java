bc0be899d5854b97f4df76580412e806e01593c1
javatarupeu <javatar.sempai@gmail.com>
2016-01-26 11:23:19 -0500

    Implementando interfaz Poligono

---
+++ POO-Ciclo2/src/pe/edu/upeu/poo/interfaz/Poligono.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.poo.interfaz;
+
+public interface Poligono {
+
+	public double calcularArea();
+	public void saludar();
+	
+}
---
+++ POO-Ciclo2/src/pe/edu/upeu/poo/interfaz/Triangulo.java
@@ -0,0 +1,17 @@
+package pe.edu.upeu.poo.interfaz;
+
+public class Triangulo implements Poligono {
+
+	@Override
+	public double calcularArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void saludar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
